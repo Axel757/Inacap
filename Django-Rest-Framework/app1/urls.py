f7f789087185c8c52d9fb7eaf68d63833d4d0146
Axel757 <axelmx7@outlook.com>
2023-11-30 11:39:03 -0300

    APIVIEW class and def

--- Django-Rest-Framework/app1/urls.py
+++ Django-Rest-Framework/app1/urls.py
@@ -2,7 +2,9 @@ from django.urls import path, include
 from app1.views import *
 urlpatterns = [
     path('employees/', employeeView, name='employees'),
-    path('students/', student_list, name='students'),
-    path('students/<int:pk>', student_detail, name='students/<pk>'),
+    # path('students/', student_list, name='students'),
+    # path('students/<int:pk>', student_detail, name='students/<pk>'),
     
+    path('students/', StudentList.as_view(), name='students'),
+    path('students/<int:pk>', StudentDetail.as_view, name='students/<pk>'),    
 ]
--- Django-Rest-Framework/app1/views.py
+++ Django-Rest-Framework/app1/views.py
@@ -18,41 +18,41 @@ def employeeView(request):
     return JsonResponse(emp)
 
 
-@api_view(['GET', 'POST'])
-def student_list(request):
-    if request.method == 'GET':
-        students = Student.objects.all()
-        serializer = StudentSerializer(students, many=True)
-        return Response(serializer.data)
+# @api_view(['GET', 'POST'])
+# def student_list(request):
+#     if request.method == 'GET':
+#         students = Student.objects.all()
+#         serializer = StudentSerializer(students, many=True)
+#         return Response(serializer.data)
     
-    if request.method == 'POST':
-        serializer = StudentSerializer(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     if request.method == 'POST':
+#         serializer = StudentSerializer(data = request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-@api_view(['GET', 'PUT', 'DELETE'])
-def student_detail(request, pk):
-    try:
-        student = Student.objects.get(pk=pk)    
-    except Student.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def student_detail(request, pk):
+#     try:
+#         student = Student.objects.get(pk=pk)    
+#     except Student.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
     
-    if request.method == 'GET':
-       serializer = StudentSerializer(student)
-       return Response(serializer.data)
+#     if request.method == 'GET':
+#        serializer = StudentSerializer(student)
+#        return Response(serializer.data)
 
-    if request.method == 'DELETE':
-       student.delete()
-       return Response(status=status.HTTP_204_NO_CONTENT)   
+#     if request.method == 'DELETE':
+#        student.delete()
+#        return Response(status=status.HTTP_204_NO_CONTENT)   
    
-    if request.method == 'PUT':
-        serializer = StudentSerializer(student, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     if request.method == 'PUT':
+#         serializer = StudentSerializer(student, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     
 class StudentList(APIView):
@@ -67,4 +67,28 @@ class StudentList(APIView):
                 serializers.save()
                 return Response(serializers.data, status=status.HTTP_201_CREATED)
             return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
+class StudentDetail(APIView):
+        def get_object(self, pk):
+            try:
+                return Student.objects.get(pk=pk)    
+            except Student.DoesNotExist:
+                return Http404
+        
+        def get(self, request, pk):
+            student = self.get_object(pk)
+            serializers = StudentSerializer(student)
+            return Response(serializers.data)            
+        
+        def put(self, request, pk):
+            student = self.get_object(pk)
+            serializers = StudentSerializer(student, data=request.data)
+            if serializers.is_valid():
+                serializers.save()
+                return Response(serializers.data)
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)      
+        
+        def delete(self, request, pk):
+            student = self.get_object(pk)
+            student.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
